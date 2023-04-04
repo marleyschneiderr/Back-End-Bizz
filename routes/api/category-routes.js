2c33294cd747cff378efe52954ded1b8e716613d
marleyschneiderr <marley33@uw.edu>
2023-04-03 23:32:52 -0700

    Added stock to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ["id", "product_name", "price", "category_id"],
+        attributes: ["id", "product_name", , "stock", "price", "category_id"],
       },
     ],
   })
